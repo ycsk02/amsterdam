3d8212e906ff8982dd52d237f100a68e96230d1a
SuKai <ycsk02@hotmail.com>
2017-02-21 15:17:32 +0800

    clear the code and file

--- amsterdam/database/connection.py
+++ amsterdam/database/connection.py
@@ -6,14 +6,6 @@ from amsterdam import settings
 
 from elasticsearch import Elasticsearch
 
-# # db settings
-# dbuser = 'root'
-# #dbpass = 'nopass'
-# #dbhost = 'localhost'
-# dbpass = 'root'
-# dbhost = '192.168.1.120'
-# dbname = 'spider'
-
 engine = create_engine("mysql://%s:%s@%s/%s?charset=utf8&use_unicode=0"
                        %(settings.DBUSER, settings.DBPASS, settings.DBHOST, settings.DBNAME),
                        echo=False,
@@ -24,6 +16,4 @@ db = scoped_session(sessionmaker(autocommit=False,
                                  bind=engine))
 Base = declarative_base()
 
-#es_servers = '192.168.1.192:9200'
-# es_servers = '192.168.1.120:9200'
 es = Elasticsearch(hosts=settings.EC_SERVERS,timeout=60)
